bd6f0c24e5640c5ee0cd7bbb5df0ae55eb0363db
jackysum <jsum528@gmail.com>
2014-02-26 17:21:28 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ink::Application.config.secret_key_base = 'eb915b9d72a50c82c43f3e3d4dca0a5a80165ad8abf8c0ad077ca3ae93600ebfbde015e5625c8b8349a145142165c53aac69272639686c7680cd6af6e9e4fb98'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ink::Application.config.session_store :cookie_store, key: '_ink_session'
