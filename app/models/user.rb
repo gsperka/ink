72f00667046f94c430209e999d1f1117b8405a7f
jackysum <jsum528@gmail.com>
2014-02-26 18:26:47 -0600

    created nav bar with basic login logout and signup. Also added validations and has secure password in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
   has_many :sketches
   has_many :votes
+
+  validates :password_digest, :presence => true, :length => { :minimum => 6 }
+  validates :email, :uniqueness => true, :presence => true
+  validates :username, :first_name, :last_name, :presence => true
 end
