0118cd32bb55d43bdf08e3e3d9fd03012c63e9a7
jackysum <jsum528@gmail.com>
2014-02-26 18:50:36 -0600

    fixed minor use of local variables for user and session, created current_user helper and utalize helper in navbar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(username: params[:session][:username])
-    if @user.authenticate(params[:session][:password])
-      session[:current_user_id] = @user.id
+    user = User.find_by(username: params[:session][:username])
+    if user.authenticate(params[:session][:password])
+      session[:current_user_id] = user.id
       redirect_to root_path
     else
       redirect_to root_path 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,10 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
   end
 
   def create
-    @user = User.new(params[:user].permit(:username, :password, :password_confirmation, :email, :first_name, :last_name))
-    @user.save!
+    user = User.new(params[:user].permit(:username, :password, :password_confirmation, :email, :first_name, :last_name))
+    user.save!
     redirect_to root_path
   end
 end
