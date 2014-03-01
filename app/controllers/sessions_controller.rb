1974865b46dc8bdabd4afec14402939a67bf8a0c
jackysum <jsum528@gmail.com>
2014-02-28 19:27:10 -0600

    added error handling for both signup and login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(username: params[:session][:username])
-    if user.authenticate(params[:session][:password])
+    @user = User.find_by(username: params[:session][:username])
+    if @user && @user.authenticate(params[:session][:password])
       session[:current_user_id] = user.id
       redirect_to root_path
     else
-      redirect_to root_path 
+      redirect_to :back, flash: {error: "Invalid email/password combination"}
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-    user = User.new(params[:user].permit(:username, :password, :password_confirmation, :email, :first_name, :last_name))
-    user.save!
-    session[:current_user_id] = user.id
-    redirect_to root_path
+    @user = User.new(params[:user].permit(:username, :password, :password_confirmation, :email, :first_name, :last_name))
+    if @user.save
+      session[:current_user_id] = user.id
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 end
