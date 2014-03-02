43e2139ae9c8eb657c59771828d8855c46f53827
jackysum <jsum528@gmail.com>
2014-03-02 14:37:19 -0600

    only counts pixels when mouse is clicked down on the canvas. bug where pointer does not match canvas location

--- app/assets/javascripts/pixal.js
+++
@@ -1,6 +0,0 @@
-var i=0;
-$('#bigbigbox').mousemove(function(event) {
-  var msg = 'mousemove() position - x : ' + event.pageX + ', y : '
-                + event.pageY + ' [counter] : ' + i++;
-  $('#msg').text(msg);
-});
