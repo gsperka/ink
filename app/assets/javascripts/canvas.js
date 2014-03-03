e0752d0c7b0e96103c82f1afda2d74effba54445
jackysum <jsum528@gmail.com>
2014-03-03 16:51:55 -0600

    complete refactoring of all sketch tools and post methods for canvas

--- app/assets/javascripts/canvas.js
+++ app/assets/javascripts/canvas.js
@@ -1,111 +1,108 @@
 var Canvas = function(el) {
-  this.el = el
-
-  this.createCanvas($("#parent-sketch-data").val());
+  this.el = el;
+  this.parentSketch = $('#parent-sketch-data').val();
+  this.createCanvas(this.parentSketch);
   this.showTools();
-}
+  this.submitSketch();
+};
 
 Canvas.prototype.createCanvas = function(parentSketch) {
   this.fabcanvas = new fabric.Canvas('canvas-partial-box', {isDrawingMode: true});
   this.fabcanvas.loadFromJSON(parentSketch, this.fabcanvas.renderAll.bind(this.fabcanvas));
   this.fabcanvas.freeDrawingBrush.width = 5;
-}
+};
 
 Canvas.prototype.showTools = function() {
   this.clearButton();
-}
+  this.brushTools();
+  this.colorTools();
+};
 
 Canvas.prototype.clearButton = function() {
-  console.log($(this.el).find('#clear-canvas'))
   $(this.el).find('#clear-canvas').on('click', function(event) {
     document.location.reload();
-  })
+  });
+};
+
+Canvas.prototype.brushTools = function() {
+  this.brush('#super-tiny-brush', 2);
+  this.brush('#tiny-brush', 5);
+  this.brush('#small-brush', 15);
+  this.brush('#medium-brush', 25);
+  this.brush('#large-brush', 35);
+};
+
+Canvas.prototype.brush = function(el, width) {
+  var self = this;
+  $(el).mousedown(function() {
+    self.fabcanvas.freeDrawingBrush.width = width;
+  });
+};
+
+Canvas.prototype.colorTools = function() {
+  this.color('#red', '#E31623');
+  this.color('#orange', '#E36B16');
+  this.color('#yellow', '#E3E316');
+  this.color('#green', '#16E341');
+  this.color('#blue', '#165EE3');
+  this.color('#purple', '#7816E3');
+  this.color('#brown', '#5F3D25');
+  this.color('#black', '#black');
+  this.color('#gray', '#838895');
+  this.color('#white', 'white');
+};
+
+Canvas.prototype.color = function(el, color) {
+  var self = this;
+  $(el).mousedown(function() {
+    self.fabcanvas.freeDrawingBrush.color = color;
+  });
+};
+
+Canvas.prototype.submitSketch = function() {
+  self = this;
+  $(this.el).find('#sketch-submit').on("click", function(){
+    var sketchInfo = JSON.stringify(self.fabcanvas);
+    if (typeof self.parentSketch === 'undefined') {
+      self.postNewTree(sketchInfo);
+    }
+    else {
+      if(self.parentSketch === sketchInfo) alert('Cannot submit without any contribution');
+      else {
+        self.postNewSketch(sketchInfo);
+      };
+    }
+  });
+}
+
+Canvas.prototype.postNewTree = function(sketchInfo) {
+  $.ajax({
+    method: "POST",
+    url: "/trees",
+    data: {sketch_json: sketchInfo},
+    dataType: 'json',
+    success: function(response){
+      window.location.replace('/trees/' + response)
+    }
+  });
+}
+
+Canvas.prototype.postNewSketch = function(sketchInfo) {
+  $.ajax({
+    method: "POST",
+    url: "/trees/" + tree_id + "/sketches",
+    data: {sketch_json: sketchInfo, parent_id: parent_id},
+    success: function(response){
+      window.location.replace('/trees/' + tree_id)
+    }
+  });
 }
 
 $(document).ready(function(){
-  var canvas = new Canvas('#format-canvas-elements')
+  var canvas = new Canvas('#format-canvas-elements');
 });
 
 
-//   var brush = fabcanvas.freeDrawingBrush.width = 5;
-//   $("#super-tiny-brush").mousedown(function(e){
-//     fabcanvas.freeDrawingBrush.width = 2;
-//   });
-//   $("#tiny-brush").mousedown(function(e){
-//     fabcanvas.freeDrawingBrush.width = 5;
-//   });
-//   $("#small-brush").mousedown(function(e){
-//     fabcanvas.freeDrawingBrush.width = 15;
-//   });
-//   $("#medium-brush").mousedown(function(){
-//     fabcanvas.freeDrawingBrush.width = 25;
-//   });
-//   $("#large-brush").mousedown(function(){
-//     fabcanvas.freeDrawingBrush.width = 35
-//   });
-//   $("#red").mousedown(function(){
-//     fabcanvas.freeDrawingBrush.color = "#E31623"
-//   });
-//   $("#yellow").mousedown(function(){
-//     fabcanvas.freeDrawingBrush.color = "#E3E316"
-//   });
-//   $("#green").mousedown(function(){
-//     fabcanvas.freeDrawingBrush.color = "#16E341"
-//   });
-//   $("#blue").mousedown(function(){
-//     fabcanvas.freeDrawingBrush.color = "#165EE3"
-//   });
-//   $("#black").mousedown(function(){
-//     fabcanvas.freeDrawingBrush.color = "black"
-//   });
-//   $("#white").mousedown(function(){
-//     fabcanvas.freeDrawingBrush.color = "white"
-//   });
-//   $("#gray").mousedown(function(){
-//     fabcanvas.freeDrawingBrush.color = "#838895"
-//   });
-//   $("#purple").mousedown(function(){
-//     fabcanvas.freeDrawingBrush.color = "#7816E3"
-//   });
-//   $("#orange").mousedown(function(){
-//     fabcanvas.freeDrawingBrush.color = "#E36B16"
-//   });
-//   $("#brown").mousedown(function(){
-//     fabcanvas.freeDrawingBrush.color = "#5F3D25"
-//   });
 
 
-
-
-//   $('#sketch-submit').on("click", function(){
-//     var sketchInfo = JSON.stringify(fabcanvas);
-//     if (typeof parentSketch === 'undefined') {
-//       var tree = {sketch_json: sketchInfo};
-//       $.ajax({
-//         method: "POST",
-//         url: "/trees",
-//         data: tree,
-//         dataType: 'json',
-//         success: function(response){
-//           window.location.replace('/trees/' + response)
-//         }
-//       });
-//     }
-
-//     else {
-//       if(parentSketch === sketchInfo) alert('Cannot submit without any contribution');
-//       else {
-//         var tree = {sketch_json: sketchInfo, parent_id: parent_id};
-//         $.ajax({
-//           method: "POST",
-//           url: "/trees/" + tree_id + "/sketches",
-//           data: tree,
-//           success: function(response){
-//             window.location.replace('/trees/' + tree_id)
-//           }
-//         });
-//       };
-//     }
-//   });
 //   if(typeof(pixelCount) === 'function') pixelCount();
-// 
