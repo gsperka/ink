8f7b460957ed54a36c428ec5236b9a3eb62485c5
Sam Blackman <sblackman@gmail.com>
2014-03-03 23:03:28 -0600

    users can start a new tree and save when logged out and have it assigned to them when logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,10 @@ class UsersController < ApplicationController
     @user = User.new(params[:user].permit(:username, :password, :password_confirmation, :email, :first_name, :last_name))
     if @user.save
       session[:current_user_id] = @user.id
+      if session[:new_user_sketch_id]
+        sketch = Sketch.find(session[:new_user_sketch_id]).update(user_id: current_user.id)
+        session[:new_user_sketch_id] = nil
+      end
       redirect_to root_path
     else
       render 'new'
