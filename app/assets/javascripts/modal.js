2021a02c6bb275a7f643b917282b415d21c0daaf
author <abc@abc.com>
2014-03-03 20:03:50 -0600

    adjusted nav styling for media queries

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,32 +1,32 @@
-var Modal = function(el) {
-  this.el = el
-  $(this.el).find('#overlay').hide();
-  $(this.el).find('#login_modal').hide();
-  $(this.el).find('#login_modal').css({'margin-left': '-' + (($('#login_modal').width() / 2) + parseInt($("#login_modal").css('padding-left'))) + 'px'});
-  $(this.el).find('#login_modal').css({'margin-top': '-' + ($('#login_modal').width() / 2) + 'px'});
-  
-  this.login();
-  this.clickOff();
-};
+// var Modal = function(el) {
+//   this.el = el
+//   $(this.el).find('#overlay').hide();
+//   $(this.el).find('#login_modal').hide();
+//   $(this.el).find('#login_modal').css({'margin-left': '-' + (($('#login_modal').width() / 2) + parseInt($("#login_modal").css('padding-left'))) + 'px'});
+//   $(this.el).find('#login_modal').css({'margin-top': '-' + ($('#login_modal').width() / 2) + 'px'});
 
-Modal.prototype.login = function() {
-  self = this.el
-  $(this.el).find('#login').on('click', function(event) {
-    event.preventDefault();
+//   this.login();
+//   this.clickOff();
+// };
 
-    $(self).find('#overlay').show();
-    $(self).find('#login_modal').show();
-  });
-};
+// Modal.prototype.login = function() {
+//   self = this.el
+//   $(this.el).find('#login').on('click', function(event) {
 
-Modal.prototype.clickOff = function() {
-  self = this.el
-  $(this.el).find('#overlay, #nav').click(function() {
-    $(self).find('#overlay').hide();
-    $(self).find('#login_modal').hide();
-  })
-}
 
-$(document).ready(function() {
-  var modal = new Modal('body');
-});
+//     $(self).find('#overlay').show();
+//     $(self).find('#login_modal').show();
+//   });
+// };
+
+// Modal.prototype.clickOff = function() {
+//   self = this.el
+//   $(this.el).find('#overlay, #nav').click(function() {
+//     $(self).find('#overlay').hide();
+//     $(self).find('#login_modal').hide();
+//   })
+// }
+
+// $(document).ready(function() {
+//   var modal = new Modal('body');
+// });
