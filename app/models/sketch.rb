8cd5dc632d842bfc78f71ba8d9655326757178de
jackysum <jsum528@gmail.com>
2014-03-04 20:30:26 -0600

    added plus to streches#show and linked it to streches#new

--- app/models/sketch.rb
+++ app/models/sketch.rb
@@ -3,4 +3,8 @@ class Sketch < ActiveRecord::Base
   belongs_to :tree, counter_cache: true
   belongs_to :parent, class_name: 'Sketch'
   has_many :children, class_name: 'Sketch', foreign_key: 'parent_id'
+
+  def self.random_sketch
+    sketch = Sketch.find(rand(self.count))
+  end
 end
